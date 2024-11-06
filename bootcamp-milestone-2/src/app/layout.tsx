0d395d3d1f16a8e9e30e6541c5804b8152332994
Brady Welsh <blwelsh@calpoly.edu>
2024-11-06 10:06:42 -0800

    feat: configured next.js and react

---
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+//import localFont from "next/font/local";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "100"
+});
+
+/*
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+*/
+
+export const metadata: Metadata = {
+  title: "Brady Welsh",
+  description: "Personal wsebsite for Brady",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
