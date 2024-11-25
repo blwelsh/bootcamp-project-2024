91a978205ffe222da2e383b98a3e7942defc2c3e
Brady Welsh <blwelsh@calpoly.edu>
2024-11-24 20:57:47 -0500

    feat: added portfolioSchema

---
+++ bootcamp-milestone-2/src/database/portfolioSchema.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+export type PortfolioObject = {
+    name: string,
+    description: string,
+    image: string,
+    imageAlt: string,
+    url: string,
+}
+
+const portfolioSchema = new Schema<PortfolioObject>({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    image: {type: String, required: true},
+    imageAlt: {type: String, required: true},
+    url: {type: String, required: true},
+});
+
+const portfolioModel = mongoose.models['portfolios'] || mongoose.model('portfolios', portfolioSchema);
+export default portfolioModel;
